f41da4377a32ec13bfc4be6bc97d0b58e2d64a83
Steven Christian Susanto <nicenicenice345@gmail.com>
2024-05-06 20:51:33 +0700

    add:bagianMaul

--- code/resources/views/dashboard/index.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>Hey, welcome {{ auth()->user()->username }}, this is the soon to be dashboard</h1>
-<form action="/logout" method="post">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
