06c107baac3a6077edf5a3947dd8dadcdc5e6c48
shofa yasmin fauziah <yasminshofa.sy@gmail.com>
2024-05-14 07:56:56 +0700

    contribution-model-controller

---
+++ P_Lime/database/migrations/2024_05_13_163358_create_contributions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contributions', function (Blueprint $table) {
+            $table->id('contribution_id');
+            $table->string('contribution_name',100);
+            $table->enum('payment_status',['Belum dibayar','Lunas']);
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contributions');
+    }
+};
